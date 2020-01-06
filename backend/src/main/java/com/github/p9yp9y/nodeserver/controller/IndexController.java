635b005fd41106c4f88d1f14af3e660f8b5a0384
Dobrosi András <dobrosi@gmail.com>
2020-01-06 18:20:16 +0100

    3rd

--- backend/src/main/java/com/github/p9yp9y/nodeserver/controller/IndexController.java
+++ backend/src/main/java/com/github/p9yp9y/nodeserver/controller/IndexController.java
@@ -2,15 +2,17 @@ package com.github.p9yp9y.nodeserver.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 	@RequestMapping(value = "/")
-	public void hello(final HttpServletResponse response) throws IOException {
-		response.getOutputStream().write("Hello".getBytes());
+	public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") final String name, final Model model)
+			throws IOException {
+		model.addAttribute("name", name);
+		return "index";
 	}
 }
