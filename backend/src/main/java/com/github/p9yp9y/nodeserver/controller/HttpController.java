35aa49960246a1a37736cb096b8b7bf180a532d1
Dobrosi András <dobrosi@gmail.com>
2020-01-06 15:33:24 +0100

    2nd

--- backend/src/main/java/com/github/p9yp9y/nodeserver/controller/HttpController.java
+++ backend/src/main/java/com/github/p9yp9y/nodeserver/controller/HttpController.java
@@ -12,21 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.github.p9yp9y.nodeserver.service.HttpService;
 
 @Controller
-<<<<<<< HEAD
 @RequestMapping(value = "/http")
 public class HttpController {
 	@Autowired
 	private HttpService httpService;
-=======
-public class HttpController {
-	@Autowired
-	private HttpService httpService;
-
-	@RequestMapping(value = "/")
-	public void hello(final HttpServletResponse response) throws IOException {
-		response.getOutputStream().write("Hello".getBytes());
-	}
->>>>>>> branch 'master' of https://github.com/p9yp9y/node_server.git
 
 	@RequestMapping(value = "/get")
 	public void get(final HttpServletResponse response, @RequestParam(name = "url", required = false) final String url) throws IOException {
