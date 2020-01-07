1a75de8031edc906cff14e3144d4b3f61ace6ed4
p9yp9y <p9yp9y@gmail.com>
2020-01-07 19:49:09 +0100

    dfdf

--- backend/src/main/java/com/github/p9yp9y/nodeserver/controller/IndexController.java
+++ backend/src/main/java/com/github/p9yp9y/nodeserver/controller/IndexController.java
@@ -6,13 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/hello")
 	public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") final String name, final Model model)
 			throws IOException {
 		model.addAttribute("name", name);
-		return "index";
+		return "hello";
+	}
+	
+	@RequestMapping(value = "/")
+	public ModelAndView index() {
+		
+		return new ModelAndView("redirect:/myhome");
 	}
 }
