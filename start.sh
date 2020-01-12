8567df3113b7caaa26aacef3f6dc08f10fcfaac4
p9yp9y <p9yp9y@gmail.com>
2020-01-12 10:01:17 +0100

    start script fix

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
-if git pull 2>&1 | grep "Current branch master is up to date."; then
-	echo "up to date";
+if [[ `git status --porcelain` ]]; then
+  # Changes
 else
+	git pull
 	cd backend
 	mvn package
 	pkill -f 'node_server'
 	java -jar -Dspring.config.location=~/.config/node_server/application.properties target/node_server-*.jar
 fi
+
 exit
