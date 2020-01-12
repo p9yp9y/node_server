a1ebe8415a0fafd70f07f45bcf44ce9b80305f4e
Your Name <you@example.com>
2020-01-12 09:41:24 +0000

    bash fix

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
-while true; do
-if [[ `git status --porcelain` ]]; then
-	echo 'no changes'
-else
-	git pull
-	cd backend
-	mvn package
+#!/bin/bash
+
+restart() {
 	pkill -f 'node_server'
-	java -jar -Dspring.config.location=~/.config/node_server/application.properties target/node_server-*.jar
-fi
-sleep 60
-done
+	java -jar -Dspring.config.location=~/.config/node_server/application.properties target/node_server-*.jar &
+}
+
+init() {
+	while true; do
+		if [[ `git status --porcelain` ]]; then
+			echo 'no changes'
+		else
+			git pull
+			mvn package
+			restart
+		fi
+		sleep 60
+	done
+}
+
+cd backend
+restart
+init
