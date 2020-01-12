e3e630d531e6fb94593c121e7123f085dc0cc2da
p9yp9y <p9yp9y@gmail.com>
2020-01-12 09:36:06 +0100

    lk;k;

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-git pull
-cd backend
-mvn package
-java -jar -Dspring.config.location=~/.config/node_server/application.properties target/*.jar
+if git pull 2>&1 | grep "Current branch master is up to date."; then
+	echo "up to date";
+else
+	cd backend
+	mvn package
+	pkill -f 'node_server'
+	java -jar -Dspring.config.location=~/.config/node_server/application.properties target/node_server-*.jar
+fi
+exit
