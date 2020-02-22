3722e3f206ccefa53822458a5409f7f615f64609
p9yp9y <p9yp9y@gmail.com>
2020-02-22 08:11:13 +0100

    Update MagicHomeController.java

--- backend/src/main/java/com/github/p9yp9y/nodeserver/controller/MagicHomeController.java
+++ backend/src/main/java/com/github/p9yp9y/nodeserver/controller/MagicHomeController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import javax.annotation.PostConstruct;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
